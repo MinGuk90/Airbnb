42bd1ee8fbe563017bcb16a1d1c5e57a436cad59
minguk <minkuk90@gmail.com>
2022-04-03 08:54:20 +0900

    Implementing the Date Picker

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,9 +7,31 @@ import {
   SearchIcon,
 } from "@heroicons/react/solid";
 import { useState } from "react";
+import "react-date-range/dist/styles.css"; // 메인 스타일 파일
+import "react-date-range/dist/theme/default.css"; // 테마 CSS 파일
+import { DateRangePicker } from "react-date-range";
 
 function Header() {
   const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = ranges => {
+    console.log(ranges);
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
 
   return (
     <header
@@ -48,6 +70,36 @@ function Header() {
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-10">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl flex-grow font-semibold">
+              Number of Guests
+            </h2>
+
+            <UsersIcon className="h-5" />
+            <input
+              value={noOfGuests}
+              onChange={e => setNoOfGuests(e.target.value)}
+              min={1}
+              type="number"
+              className="w-12 pl-2 text-lg outling-none text-red-400"
+            />
+          </div>
+          <div className="flex ">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancle
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
